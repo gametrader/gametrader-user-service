526a32b02792003c924a2ab81f0f5cb9ce779471
ptrrk1337 <mymyry1999@gmail.com>
2022-03-06 18:39:17 +0100

    GAMETRADER-41 Initialized user service

---
+++ src/main/java/com/gametrader/gametraderuserservice/GametraderUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gametrader.gametraderuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GametraderUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GametraderUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gametrader/gametraderuserservice/GametraderUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gametrader.gametraderuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GametraderUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
