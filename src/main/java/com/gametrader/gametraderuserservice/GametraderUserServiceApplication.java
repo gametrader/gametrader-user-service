2d023d6960d9a269a6a2b3f7be06ea7df0db017a
ptrrk1337 <mymyry1999gmail.com>
2022-05-21 13:10:40 +0200

    Eureka hotfix

--- src/main/java/com/gametrader/gametraderuserservice/GametraderUserServiceApplication.java
+++ src/main/java/com/gametrader/gametraderuserservice/GametraderUserServiceApplication.java
@@ -2,8 +2,10 @@ package com.gametrader.gametraderuserservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class GametraderUserServiceApplication {
 
 	public static void main(String[] args) {
